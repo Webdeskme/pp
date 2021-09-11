62c4749c4376bf3f5c3cccd378db760679ca2bb8
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-11 08:42:39 -0400

    test

--- game/index.js
+++ game/index.js
@@ -129,8 +129,12 @@ var TitleS = new Phaser.Class({
 });
 var config = {
     type: Phaser.AUTO,
-    mode: Phaser.Scale.FIT,
-    parent: "app",
+    scale: {
+      mode: Phaser.Scale.FIT,
+      parent: "app",
+      width: 1024,
+      height: 640
+    },
     width: 800,
     height: 600,
     backgroundColor: '#000000',
