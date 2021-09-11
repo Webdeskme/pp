91f9504d693f52b933c5879eecadedcfe654e978
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-11 15:12:52 -0400

    fix

--- game/index.js
+++ game/index.js
@@ -215,7 +215,12 @@ var P1 = new Phaser.Class({
           function bselect(c, th, im){
             c.on('pointerdown',function(pointer){
               if(red == "yes"){
-                c.setTint(0x00ff00);
+                if(tintFill(true)){
+                  c.clearTint();
+                }
+                else{
+                  c.setTint(0x00ff00);
+                }
               }
             });
           }
