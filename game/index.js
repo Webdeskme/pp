05abeaf9ff8b0b424cf281ad58d49f79ac40cdc3
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-14 15:33:38 -0400

    lose

--- game/index.js
+++ game/index.js
@@ -371,7 +371,7 @@ var P1 = new Phaser.Class({
                     //console.log(temp);
                   }
                   else if (c["texture"]["key"] == "P1S") {
-                    skill += 6;
+                    skill += 1;
                     card += 1;
                     temp.push(c["texture"]["key"]);
                     //console.log(temp);
@@ -1465,6 +1465,10 @@ var P1 = new Phaser.Class({
           else if (c0["texture"]["key"] == "MFAB") {
             health -= 1;
           }
+          if(health < 1){
+              this.sound.add('click').play();
+              this.scene.start('loseText');
+          }
           healthText.setText('Health:' + health);
           var t = cards.indexOf(c0["texture"]["key"]);
           cards.splice(t, 1);
@@ -1696,6 +1700,186 @@ var P1 = new Phaser.Class({
     });
     }
 });
+var Lose = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function Lose ()
+    {
+        Phaser.Scene.call(this, { key: 'lose' });
+    },
+
+    preload: function ()
+    {
+        var cards = ["Browser", "Malware", "PrivilegeEsc", "Driver", "Service", "CloudStorage", "IMDS", "MFA", "User", "CloudAccount", "CredStuffing", "Kubelet", "WateringHole", "WebShell", "FakeInstaller", "OfficeMacro", "ShadowCopy", "Sysmon", "Behavior", "DPAT", "Endpoint", "SIEM", "Sinkhole", "ZeroTrust", "IAM", "SRUM", "MFAB"];
+        //this.load.image('back', 'assets/backgrounds/back1.jpg');
+        this.load.image('back', 'assets/backgrounds/star1.jpg');
+        this.load.image('title', 'assets/Text/text.png');
+        this.load.image('cb', 'assets/out/back.png');
+        var i;
+        for (i = 0; i < cards.length; i++) {
+          this.load.image(cards[i], 'assets/out/' + cards[i] + '.png');
+        }
+        this.load.audio('click', [
+        "assets/Audio/mouseclick.wav"
+        ]);
+    },
+
+    create: function ()
+    {
+      //this.scale.startFullscreen();
+      function shuffle(array) {
+          var currentIndex = array.length, temporaryValue, randomIndex;
+
+          // While there remain elements to shuffle...
+          while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+          }
+
+          return array;
+          }
+        //var cards = ["s10", "s11", "s12", "s13", "s14", "s15", "s16","s17","s18", "s20", "s21", "s22", "s23", "s24", "s25", "s26","s27","s28","s30", "s31", "s32", "s33", "s34", "s35", "s36","s37","s38", "s40", "s41", "s42", "s43", "s44", "s45", "s46","s47","s48","s50", "s51", "s52", "s53", "s54", "s55", "s56","s57","s58","s60", "s61", "s62", "s63", "s64", "s65", "s66","s67","s68"];
+        var cards = ["Browser", "Malware", "PrivilegeEsc", "Driver", "Service", "CloudStorage", "IMDS", "MFA", "User", "CloudAccount", "CredStuffing", "Kubelet", "WateringHole", "WebShell", "FakeInstaller", "OfficeMacro", "ShadowCopy", "Sysmon", "Behavior", "DPAT", "Endpoint", "SIEM", "Sinkhole", "ZeroTrust", "IAM", "SRUM", "MFAB"];
+        shuffle(cards);
+        this.add.image(512, 320, 'back');
+        var i;
+        var c = this.add.image(250, 300, 'cb');
+        c.setScale(1/2);
+        var loseText = this.add.text(15, 65, 'Defeat!', { fontSize: '64px', fill: '#fff' });
+        for (i = 0; i < cards.length; i++) {
+          var c = this.add.image(250, 300, cards[i]).setInteractive({ useHandCursor: true  } );
+          c.setScale(1/2);
+          //this.input.setDraggable(c);
+          this.input.on('gameobjectdown', function (pointer, gameObject) {
+              //this.sound.add('click').play();
+              gameObject.destroy();
+
+          });
+        }
+        var d = this.add.image(700, 100, 'title');
+        d.setInteractive({ useHandCursor: true  } );
+        d.setScale(1/4);
+        d.on('pointerover',function(pointer){
+          //console.log('hover');
+          d.setScale(1/3);
+        });
+        d.on('pointerout',function(pointer){
+          //console.log('out');
+          d.setScale(1/4);
+        });
+        d.on('pointerdown', () => {
+          this.sound.add('click').play();
+          //console.log('Video');
+          this.scene.start('p1');
+        });
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+
+    });
+    }
+});
+var Win = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function Win ()
+    {
+        Phaser.Scene.call(this, { key: 'win' });
+    },
+
+    preload: function ()
+    {
+        var cards = ["Browser", "Malware", "PrivilegeEsc", "Driver", "Service", "CloudStorage", "IMDS", "MFA", "User", "CloudAccount", "CredStuffing", "Kubelet", "WateringHole", "WebShell", "FakeInstaller", "OfficeMacro", "ShadowCopy", "Sysmon", "Behavior", "DPAT", "Endpoint", "SIEM", "Sinkhole", "ZeroTrust", "IAM", "SRUM", "MFAB"];
+        //this.load.image('back', 'assets/backgrounds/back1.jpg');
+        this.load.image('back', 'assets/backgrounds/star1.jpg');
+        this.load.image('title', 'assets/Text/text.png');
+        this.load.image('cb', 'assets/out/back.png');
+        var i;
+        for (i = 0; i < cards.length; i++) {
+          this.load.image(cards[i], 'assets/out/' + cards[i] + '.png');
+        }
+        this.load.audio('click', [
+        "assets/Audio/mouseclick.wav"
+        ]);
+    },
+
+    create: function ()
+    {
+      //this.scale.startFullscreen();
+      function shuffle(array) {
+          var currentIndex = array.length, temporaryValue, randomIndex;
+
+          // While there remain elements to shuffle...
+          while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+          }
+
+          return array;
+          }
+        //var cards = ["s10", "s11", "s12", "s13", "s14", "s15", "s16","s17","s18", "s20", "s21", "s22", "s23", "s24", "s25", "s26","s27","s28","s30", "s31", "s32", "s33", "s34", "s35", "s36","s37","s38", "s40", "s41", "s42", "s43", "s44", "s45", "s46","s47","s48","s50", "s51", "s52", "s53", "s54", "s55", "s56","s57","s58","s60", "s61", "s62", "s63", "s64", "s65", "s66","s67","s68"];
+        var cards = ["Browser", "Malware", "PrivilegeEsc", "Driver", "Service", "CloudStorage", "IMDS", "MFA", "User", "CloudAccount", "CredStuffing", "Kubelet", "WateringHole", "WebShell", "FakeInstaller", "OfficeMacro", "ShadowCopy", "Sysmon", "Behavior", "DPAT", "Endpoint", "SIEM", "Sinkhole", "ZeroTrust", "IAM", "SRUM", "MFAB"];
+        shuffle(cards);
+        this.add.image(512, 320, 'back');
+        var i;
+        var c = this.add.image(250, 300, 'cb');
+        c.setScale(1/2);
+        var winText = this.add.text(15, 65, 'Victory!', { fontSize: '64px', fill: '#fff' });
+        for (i = 0; i < cards.length; i++) {
+          var c = this.add.image(250, 300, cards[i]).setInteractive({ useHandCursor: true  } );
+          c.setScale(1/2);
+          //this.input.setDraggable(c);
+          this.input.on('gameobjectdown', function (pointer, gameObject) {
+              //this.sound.add('click').play();
+              gameObject.destroy();
+
+          });
+        }
+        var d = this.add.image(700, 100, 'title');
+        d.setInteractive({ useHandCursor: true  } );
+        d.setScale(1/4);
+        d.on('pointerover',function(pointer){
+          //console.log('hover');
+          d.setScale(1/3);
+        });
+        d.on('pointerout',function(pointer){
+          //console.log('out');
+          d.setScale(1/4);
+        });
+        d.on('pointerdown', () => {
+          this.sound.add('click').play();
+          //console.log('Video');
+          this.scene.start('p1');
+        });
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+
+    });
+    }
+});
 /*
 scale: {
   mode: Phaser.Scale.FIT,
@@ -1709,7 +1893,7 @@ var config = {
     width: 1024,
     height: 640,
     backgroundColor: '#000000',
-    scene: [ Vide, TitleS, P1 ]
+    scene: [ Vide, TitleS, P1, Lose, Win ]
 };
 
 var game = new Phaser.Game(config);
