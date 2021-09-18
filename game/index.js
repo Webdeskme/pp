f6961acacdcbd371e2c11119408f6b19f8924498
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-18 05:34:05 -0400

    starfield

--- game/index.js
+++ game/index.js
@@ -2121,6 +2121,24 @@ var P1 = new Phaser.Class({
         shuffle(cards);
         shuffle(p);
         back = this.add.image(512, 320, 'back');
+
+        ////////////////// Starfield ////////////////////////
+
+
+        this.points = [];
+        this.stars = this.add.group();
+        this.maxDepth = 32;
+
+        for (var i = 0; i < 512; i++) {
+            this.points.push({
+                x: Phaser.Math.Between(-25, 25),
+                y: Phaser.Math.Between(-25, 25),
+                z: Phaser.Math.Between(1, this.maxDepth)
+            });
+        }
+
+
+        ////////////////// End Starfield /////////////////////
         var i;
         for (i = 6; i < cards.length; i++) {
           var c = this.add.image(125, 400, cards[i]).setInteractive({ useHandCursor: true  } );
@@ -2625,6 +2643,34 @@ var P1 = new Phaser.Class({
       gameObject.y = dragY;
 
     });
+  },
+  update() {
+        this.stars.clear(true, true);
+        for (var i = 0; i < this.points.length; i++) {
+            var point = this.points[i];
+
+            point.z -= 0.2;
+
+            if (point.z <= 0) {
+                point.x = Phaser.Math.Between(-25, 25);
+                point.y = Phaser.Math.Between(-25, 25);
+                point.z = this.maxDepth;
+            }
+
+            var px = point.x * (128 / point.z) + (this.game.config.width * 0.5);
+            var py = point.y * (128 / point.z) + (this.game.config.height * 0.5);
+
+            var circle = new Phaser.Geom.Circle(
+                px,
+                py,
+                (1 - point.z / 32) * 2
+            );
+
+            var graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
+            graphics.setAlpha((1 - point.z / 32));
+            graphics.fillCircleShape(circle);
+            this.stars.add(graphics);
+        }
     }
 });
 var Lose = new Phaser.Class({
