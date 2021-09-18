34e146e6b6489d98c7ea57d9caf191270776942c
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-18 05:57:01 -0400

    test

--- game/index.js
+++ game/index.js
@@ -2667,7 +2667,7 @@ var P1 = new Phaser.Class({
             );
 
             var graphics = this.add.graphics({ fillStyle: { color: 0xffffff } });
-            graphics.setAlpha((1 - point.z ));
+            graphics.setAlpha((1 - point.z / 8));
             graphics.fillCircleShape(circle);
             this.stars.add(graphics);
         }
