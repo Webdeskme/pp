c990278be10aab0abe1812284d9c840bca8f75c8
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-20 16:19:14 -0400

    fix

--- game/index.js
+++ game/index.js
@@ -490,9 +490,9 @@ var TitleS = new Phaser.Class({
           });
         }
         var d = this.add.image(900, 100, 'title').setScrollFactor(0,0);
-        var e = this.add.image(900, 200, 'videol').setScrollFactor(0,0);
-        var f = this.add.image(900, 300, 'story').setScrollFactor(0,0);
-        var g = this.add.image(900, 400, 'puzzle').setScrollFactor(0,0);
+        var e = this.add.image(900, 150, 'videol').setScrollFactor(0,0);
+        var f = this.add.image(900, 200, 'story').setScrollFactor(0,0);
+        var g = this.add.image(900, 250, 'puzzle').setScrollFactor(0,0);
         d.setInteractive({ useHandCursor: true  } );
         d.setScale(1/4);
         e.setInteractive({ useHandCursor: true  } );
