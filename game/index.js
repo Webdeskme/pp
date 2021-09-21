0eeac094cef3d83dec5bd783af630ee0115c1bea
Webdesk-me <84015067+Senary-Deck@users.noreply.github.com>
2021-09-21 16:37:17 -0400

    fix

--- game/index.js
+++ game/index.js
@@ -288,10 +288,10 @@ var Puzz = new Phaser.Class({
 
           return array;
           }
-          shuffle(p);
       this.game.sound.stopAll();
       this.add.image(512, 320, 'back');
       var p = ['sans1', 'sans2', 'sans3', 'sans4', 'sans5', 'sans6', 'sans7', 'sans8', 'sans9', 'sans10', 'sans11', 'sans12', 'sans13', 'sans14', 'sans15', 'sans16'];
+      shuffle(p);
       var i;
       for (let i = 0; i < p.length; i++) {
         var cc = this.add.image(512, 320, p[i]).setInteractive({ useHandCursor: true  } );
