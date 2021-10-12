cb25be4a5248f0871f7744b5b30a797d885cc60a
Webdesk-me <atelford@webdesk.me>
2021-10-12 14:16:03 -0400

    update m

--- game/game.js
+++ game/game.js
@@ -965,6 +965,7 @@ var SubM = new Phaser.Class({
           this.load.image('bob', 'assets/Text/bob.png');
           this.load.image('alice', 'assets/Text/alice.png');
           this.load.image('2play', 'assets/Text/2play.png');
+          this.load.image('ba', 'assets/Text/ba.png');
           this.load.text('q1', 'assets/Text/quote/q1.txt');
           this.load.text('q2', 'assets/Text/quote/q2.txt');
           this.load.text('q3', 'assets/Text/quote/q3.txt');
@@ -1034,6 +1035,7 @@ var SubM = new Phaser.Class({
           var bb = this.add.image(900, 150, '2play').setScrollFactor(0,0);
           var aa = this.add.image(900, 200, 'bob').setScrollFactor(0,0);
           var p = this.add.image(900, 250, 'alice').setScrollFactor(0,0);
+          var ba = this.add.image(900, 300, 'ba').setScrollFactor(0,0);
           un.setInteractive({ useHandCursor: true  } );
           un.setScale(1/4);
           bb.setInteractive({ useHandCursor: true  } );
@@ -1042,6 +1044,8 @@ var SubM = new Phaser.Class({
           aa.setScale(1/4);
           p.setInteractive({ useHandCursor: true  } );
           p.setScale(1/4);
+          ba.setInteractive({ useHandCursor: true  } );
+          ba.setScale(1/4);
           un.on('pointerover',function(pointer){
             un.setScale(1/3);
           });
@@ -1073,6 +1077,12 @@ var SubM = new Phaser.Class({
             this.scene.start('subm');
             //window.location.href = "vscomp.html";
           });
+          ba.on('pointerover',function(pointer){
+            ba.setScale(1/3);
+          });
+          ba.on('pointerout',function(pointer){
+            ba.setScale(1/4);
+          });
           bb.on('pointerdown', () => {
             this.sound.add('click').play();
             //endm.stop();
@@ -1093,6 +1103,13 @@ var SubM = new Phaser.Class({
             this.game.sound.stopAll();
             //this.scene.start('p1');
             window.location.href = "vscomph.html";
+          });
+          ba.on('pointerdown', () => {
+            this.sound.add('click').play();
+            //endm.stop();
+            this.game.sound.stopAll();
+            //this.scene.start('p1');
+            window.location.href = "vscompa.html";
           });
           }
         });
