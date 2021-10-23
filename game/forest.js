64e505a2accc5ed07ab5e47a37ace019aa7b1819
Webdesk-me <atelford@webdesk.me>
2021-10-23 10:12:51 -0400

    init forest

---
+++ game/forest.js
@@ -0,0 +1,112 @@
+var P1 = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+    function P1 ()
+    {
+        Phaser.Scene.call(this, { key: 'p1' });
+    },
+    preload: function ()
+    {
+        this.load.bitmapFont('topaz', 'assets/fonts/lato.png', 'assets/fonts/lato.xml');
+        this.load.image('back1', 'assets/backgrounds/parallax-forest-back-trees.png');
+        this.load.image('back2', 'assets/backgrounds/parallax-forest-lights.png');
+        this.load.image('back3', 'assets/backgrounds/parallax-forest-middle-trees.png');
+        this.load.image('back4', 'assets/backgrounds/parallax-forest-front-trees.png');
+        this.load.image('back1', 'assets/backgrounds/parallax-forest-back-trees.png');
+        this.load.image('sans', 'assets/player/bldg.png');
+        this.load.spritesheet('walk', 'assets/player/shq_trooper_run_strip6.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('idle', 'assets/player/shq_trooper_idle_strip2.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('jump', 'assets/player/shq_trooper_jump_strip4.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.audio('music', [
+        "assets/Audio/MyVeryOwnDeadShip.mp3"
+        ]);
+    },
+    create: function ()
+    {
+      this.add.image(512, 320, 'back1');
+      this.add.image(512, 320, 'back2');
+      this.add.image(512, 320, 'back3');
+      this.add.image(512, 320, 'back4');
+      sans  = this.add.image(1000, 600, 'sans');
+      player = this.physics.add.sprite(25, 600, 'idle');
+      player.setBounce(0.2);
+      player.setCollideWorldBounds(true);
+      this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNumbers('walk', { start: 0, end: 5 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('idle', { start: 0, end: 1 }),
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.overlap(player, sans, collectStar, null, this);
+    },
+    update() {
+      if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('walk', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('walk', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('idle');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+
+        player.anims.play('jump');
+    }
+    }
+    function collectStar (player, sans)
+{
+    //star.disableBody(true, true);
+}
+});
+var config = {
+    type: Phaser.AUTO,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      parent: "app",
+      width: 1024,
+      height: 640,
+      autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    render: {
+      clearBeforeRender: false
+    },
+     enableDebug: false,
+    backgroundColor: '#000000',
+    scene: [ P1 ]
+};
+var game = new Phaser.Game(config);
