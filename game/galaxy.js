c631af26235ee515fae95d7fa8a986b3af9535c6
Webdesk-me <atelford@webdesk.me>
2021-10-25 06:57:43 -0400

    fix

--- game/galaxy.js
+++ game/galaxy.js
@@ -33,7 +33,7 @@ var P1 = new Phaser.Class({
       //fire;
       ship = this.physics.add.image(512, 320, 'space', 'ship').setDepth(2);
       ship.enableBody = true;
-      ship.scale(1/2);
+      ship.setScale(1/2);
       ship.setBounce(0.2);
       ship.setCollideWorldBounds(true);
       var Bullet = new Phaser.Class({
