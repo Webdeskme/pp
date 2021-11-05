5f35cbbb102686e3193af0161dbcac41e9706f13
Webdesk-me <atelford@webdesk.me>
2021-11-05 15:55:07 -0400

    Update ship.js

--- game/ship.js
+++ game/ship.js
@@ -1,3 +1,4 @@
+dir = "/";
 $('body').terminal({
     hello: function(what) {
         this.echo('Hello, ' + what +
@@ -5,7 +6,15 @@ $('body').terminal({
     },
     exit: function() {
       window.location.href = "game.html";
+    },
+    help: function() {
+      this.echo('ls: list\ncd "dirname": change directories\ncat "filename": to read a file');
+    },
+    ls: function() {
+      if(dir = "/"){
+        this.echo('inbox\nnavigation\nserver_info');
+      }
     }
 }, {
-    greetings: 'Ship: SANS504 \nChief Security Officer \nls to start. \ntype exit to exit'
+    greetings: 'Ship: SANS504 \nChief Security Officer \nhelp to start. \ntype exit to exit'
 });
