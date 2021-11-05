3a53636700ec078d1e642b711a9008399f6d2f55
Webdesk-me <atelford@webdesk.me>
2021-11-05 18:12:36 -0400

    Update ship.js

--- game/ship.js
+++ game/ship.js
@@ -1,4 +1,5 @@
 dir = "/";
+inbox = "Rescue.txt";
 $('body').terminal({
     hello: function(what) {
         this.echo('Hello, ' + what +
@@ -8,26 +9,41 @@ $('body').terminal({
       window.location.href = "game.html";
     },
     help: function() {
-      this.echo('ls: list\ncd "dirname": change directories\ncd "../": to go back\ncat "filename": to read a file');
+      this.echo('ls: list\ncd "dirname": change directories\ncd "../": to go back\ncat "filename": to read a file\npwd: current directory');
     },
     ls: function() {
       if(dir == "/"){
         this.echo('inbox\nnavigation\nserver_info');
       }
+      else if(dir == "/inbox"){
+        this.echo(inbox);
+      }
     },
     cd: function(d) {
       if(dir == "/"){
         if(d == "inbox"){
-          this.echo('');
+          dir = "/inbox";
         }
         else if(d == "navigation"){
-          this.echo('');
+          dir = "/navigation";
         }
         else if(d == "server_info"){
-          this.echo('');
+          dir = "/server_info";
         }
         else{
           this.echo('Error: Location not found.');
+        }
+      }
+      else if(dir == "/inbox" || dir == "/navigation"){
+        if(d == "../"){
+          dir = "/"
+        }
+      }
+    }
+    cat: function(f) {
+      if(dir == "/inbox"){
+        if(f == "Rescue.txt"){
+          this.echo('Internal Comunication\nFrom: The Captain\nTo: The Chief Security Officer\nSubject:Rescue\nI believe the pirates were able to take some important data of the ship. We must locate the pirates and take back that data before it falls into the wrong hand. You are in charge of this mission and the ships navigation is in your hands.');
         }
       }
     }
