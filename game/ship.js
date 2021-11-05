dd279a4dd4ea79395e68967fd4585b94bc145f20
Webdesk-me <atelford@webdesk.me>
2021-11-05 15:32:21 -0400

    Update ship.js

--- game/ship.js
+++ game/ship.js
@@ -2,6 +2,9 @@ $('body').terminal({
     hello: function(what) {
         this.echo('Hello, ' + what +
                   '. Wellcome to this terminal.');
+    },
+    exit: function() {
+      window.location.href = "game.html";
     }
 }, {
     greetings: 'Ship: SANS504 \nChief Security Officer \nls to start. \ntype exit to exit';
