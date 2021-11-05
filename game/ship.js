d4b5990f885dc69ff9ff492efb9d8e14e119b804
Webdesk-me <atelford@webdesk.me>
2021-11-05 15:03:32 -0400

    test

---
+++ game/ship.js
@@ -0,0 +1,10 @@
+<script>
+$('body').terminal({
+    hello: function(what) {
+        this.echo('Hello, ' + what +
+                  '. Wellcome to this terminal.');
+    }
+}, {
+    greetings: 'My First Web Terminal'
+});
+</script>
